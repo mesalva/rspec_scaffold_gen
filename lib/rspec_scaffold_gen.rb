554d326d6c8ef43ab15aa1f5719ba40aa1ca58b4
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2016-01-12 18:21:40 -0200

    default controller spec

--- lib/rspec_scaffold_gen.rb
+++ lib/rspec_scaffold_gen.rb
@@ -1,2 +1,9 @@
 module RspecScaffoldGen
+
+	def generate_controller(model_name, fields={})
+		template = File.read(File.expand_path("../rspec_scaffold_gen/scaffold_controller.txt.erb", __FILE__))
+		@model_class_name = model_name.classify
+		@model_name = model_name
+		ERB.new(template).result(binding)
+	end
 end
--- test/rspec_scaffold_gen_test.rb
+++ test/rspec_scaffold_gen_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class RspecScaffoldGenTest < ActiveSupport::TestCase
+
+  include RspecScaffoldGen
+
   test "truth" do
     assert_kind_of Module, RspecScaffoldGen
+  end
+
+  test "generate controller" do
+  	p generate_controller("content_group_item",{})
   end
 end
